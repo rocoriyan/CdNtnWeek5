6c08179b7c8050f3154c648755efce59e2b2caaa
rocoriyan <rcorrigan.other+github@gmail.com>
2024-01-26 12:54:40 +0000

    to-do functionality, added padding to calcDisplay & trying to implement index notation + rounding to expression on display for readability

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { evaluate } from 'mathjs'
+import { evaluate, index } from 'mathjs'
 import '../All.css'
 import { Link } from "react-router-dom";
 
@@ -20,7 +20,29 @@ function Calculator() {
     expression = expression.replace("÷", "/");
     expression = expression.replace("–", "-");
     let evaluatedSum = evaluate(expression);
+    if(evaluatedSum.length > 12){ //12 characters is max width otherwise some numbers go off the screen and cant be read
+      //let evaluatedSum = convertToIN(roundValue(evaluatedSum)); //format the number in a readable way
+    }
     return evaluatedSum;
+  }
+
+  function convertToIN(expression){//convert to index notation i.e. 2,400,000 = 2.40 * 10^6, 0.0456 = 4.56 * 10^-2
+    /*
+    let periodLocation = expression.indexOf(".");
+    let firstSF = //location of first sig fig. iterate through array maybe? or search using regex maybe
+    let desiredPeriodLocation = firstSF + 1 //we want period to be in the location after that one.
+    let powerOf = periodLocation - desiredPeriodLocation; //this is the power of 10 in the index notation
+
+    indexNotated = `${expression[firstSF]}.${expression[firstSF+1]}${expression[firstSF+1]} × 10^${powerOf}`
+    return indexNotated;
+    */
+  }
+
+  function roundValue(expression){//the aim here is to make the expression not stupidly long. so "6.6664732583622743" -> "6.67 to 3 s.f". will be combined with convertToIN to make input look nice. this function will be performed first
+    /*
+    let periodLocation = expression.indexOf(".");
+    roundedValue = parseFloat(number.toPrecision(3)) //3 sig fig
+    */
   }
 
   const buttonValues = ["7","8","9","÷","4","5","6","×","1","2","3","–","C","0","+","="];
--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,11 +1,54 @@
 import { useState } from 'react'
 import { evaluate } from 'mathjs'
 import '../All.css'
+import { useRef } from 'react';
 
 function Todo() {
+
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (addedTask) => {
+    let tasksCopy = [...tasks];
+    tasksCopy.push(addedTask);
+    setTasks(tasksCopy);
+  }
+
+  const removeTask = (removedTask) => {
+    let tasksCopy = [...tasks];
+    tasksCopy.splice(index, 1);
+    setTasks(tasksCopy);
+  }
+
+  const taskInputRef = useRef(null);
+
+  const handleClick = e => {
+    addTask(taskInputRef.current.value);
+  }
+
   return (
     <div className="main todo">
       <h2>To-do List</h2>
+      <div className="taskHolder">
+        {tasks.map((task, index) => {
+          return <ListedTasks key={index} textContent={task} removeTask={() => removeTask(index)}/>
+        })}
+      </div>
+      <input
+        type="text"
+        id="taskInput"
+        name="taskInput"
+        ref={taskInputRef}
+      />
+      <button onClick={handleClick}>Add task</button>
+    </div>
+  )
+}
+
+export const ListedTasks = (props) => {
+  return (
+    <div class="listedTask">
+      <p>{props.textContent}</p>
+      <button onClick={props.removeTask}>x</button>
     </div>
   )
 }
