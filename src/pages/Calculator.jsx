5087890b0d6f17c292072197f26d21e1a519d7a9
rocoriyan <rcorrigan.other+github@gmail.com>
2024-01-28 19:56:26 +0000

    reverted to 291fbd2 as i broke something and accidentally deployed

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -26,7 +26,7 @@ function Calculator() {
 
   function toggleMute(){
     setIsMuted(!isMuted);
-    setVolume(isMuted ? (0) : (100));
+    setVolume(isMuted ? (0) : (1));
   }
 
   function playClick(clickName){
